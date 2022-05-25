2829a772abd6e1c7bda18a92ee4ff6c7f6cf7e59
faeshal_ <faeshal@faeshals-MacBook-Pro.local>
2022-05-25 21:03:25 +0700

    migrate vendor using gcs

--- application/views/add_records.php
+++ application/views/add_records.php
@@ -14,21 +14,21 @@
 
     <!-- Fontfaces CSS-->
     <link href="https://storage.googleapis.com/ember-personal/assets/css/font-face.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
     <!-- Bootstrap CSS-->
-    <link href="<?php echo base_url(); ?>assets/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
 
     <!-- Vendor CSS-->
-    <link href="<?php echo base_url(); ?>assets/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/wow/animate.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/slick/slick.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/ember-personal/assets/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
 
     <!-- Main CSS-->
     <link href="https://storage.googleapis.com/ember-personal/assets/css/theme.css" rel="stylesheet" media="all">
@@ -190,24 +190,24 @@
     </div>
 
     <!-- Jquery JS-->
-    <script src="<?php echo base_url(); ?>assets/vendor/jquery-3.2.1.min.js"></script>
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/jquery-3.2.1.min.js"></script>
     <!-- Bootstrap JS-->
-    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap-4.1/popper.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/bootstrap-4.1/bootstrap.min.js"></script>
     <!-- Vendor JS       -->
-    <script src="<?php echo base_url(); ?>assets/vendor/slick/slick.min.js">
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/slick/slick.min.js">
     </script>
-    <script src="<?php echo base_url(); ?>assets/vendor/wow/wow.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/animsition/animsition.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/wow/wow.min.js"></script>
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/animsition/animsition.min.js"></script>
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
     </script>
-    <script src="<?php echo base_url(); ?>assets/vendor/counter-up/jquery.waypoints.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/counter-up/jquery.counterup.min.js">
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/counter-up/jquery.counterup.min.js">
     </script>
-    <script src="<?php echo base_url(); ?>assets/vendor/circle-progress/circle-progress.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/chartjs/Chart.bundle.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/select2/select2.min.js">
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="https://storage.googleapis.com/ember-personal/assets/vendor/select2/select2.min.js">
     </script>
 
     <!-- Main JS-->
